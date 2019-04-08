edd89398b3b4fb357221eb20d7e2ccfa5d836a0c
Daniel Shrimpton <44175578+DanielShrimpton@users.noreply.github.com>
2019-04-08 16:31:18 +0100

    Fixed files according to updated Ruleset

--- client/client.js
+++ client/client.js
@@ -8,7 +8,7 @@
 ////  .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
 //  .then(data => document.getElementById('content').innerHTML = data.tempo)
 //  .catch(error => console.error(error));
-// 
+//
 // function postData(url = ''){
 //   return fetch(url, {
 //     headers: {
@@ -22,43 +22,51 @@
 document.getElementById('send_btn').onclick = search; // This gets the id of the submit button and onclick will run the function search()
 
 function search(){
-  submit() // This runs a function which returns an API request for songs with the title of the search bar
 
-  // I found this example to create a table from this website
-  // https://www.encodedna.com/javascript/populate-json-data-to-html-table-using-javascript.htm
+	submit() // This runs a function which returns an API request for songs with the title of the search bar
 
-    .then(function(data) {
-      var table = document.createElement('table');
+	// I found this example to create a table from this website
+	// https://www.encodedna.com/javascript/populate-json-data-to-html-table-using-javascript.htm
 
-      for (var i = 0; i < data.tracks.items.length; i++){
-        tr = table.insertRow(-1);
+		.then(function(data) {
 
-        var tabCell = tr.insertCell(-1);
-        tabCell.innerHTML = data.tracks.items[i].name;
+			var table = document.createElement('table');
 
-        var tabCell = tr.insertCell(-1);
-        tabCell.innerHTML = data.tracks.items[i].album.artists[0].name;
+			for (var i = 0; i < data.tracks.items.length; i++){
 
-        var tabCell = tr.insertCell(-1);
-        tabCell.innerHTML = data.tracks.items[i].album.name;
-      }
+				var tr = table.insertRow(-1);
 
-      var divContainer = document.getElementById('content');
-      divContainer.innerHTML = '';
-      divContainer.appendChild(table);
-    })
-    .catch(error => console.error(error));
+				var tabCell = tr.insertCell(-1);
+				tabCell.innerHTML = data.tracks.items[i].name;
+
+				// var tabCell = tr.insertCell(-1);
+				tabCell.innerHTML = data.tracks.items[i].album.artists[0].name;
+
+				// var tabCell = tr.insertCell(-1);
+				tabCell.innerHTML = data.tracks.items[i].album.name;
+
+			}
+
+			var divContainer = document.getElementById('content');
+			divContainer.innerHTML = '';
+			divContainer.appendChild(table);
+
+		})
+		.catch(error => console.error(error));
+
 }
 
 function submit(){
-  var search = document.getElementById('txt_field').value; // This gets the value of the search bar
-  var url = 'https://api.spotify.com/v1/search?q=name:'+search+'&type=track'; // Combines the search URL and the search criteria
 
-  return fetch(url, {
-    headers: {
-      'Authorization': 'Bearer BQCKyRsLzOGGbWmDnz9L864u-IDkCZSV02KBWc0zhLqj_F0eu_5bezhyZLs22haUXmLV10hV-nv8SbZVcZxF0J6CP2HBd8RtOX7eTBQMqPUwpJ1VfkbRZ6Ylx4yqg1igX0VCtapT-8P6WIQKwNnqsiEcAwk83EIhm6pxEjlx-k-WyVj_27boyV429IACtAuK9XcFSQ'
-    },
-    body: JSON.stringify()
-  })
-    .then(response => response.json());
+	var search = document.getElementById('txt_field').value; // This gets the value of the search bar
+	var url = 'https://api.spotify.com/v1/search?q=name:'+search+'&type=track'; // Combines the search URL and the search criteria
+
+	return fetch(url, {
+		headers: {
+			'Authorization': 'Bearer BQCKyRsLzOGGbWmDnz9L864u-IDkCZSV02KBWc0zhLqj_F0eu_5bezhyZLs22haUXmLV10hV-nv8SbZVcZxF0J6CP2HBd8RtOX7eTBQMqPUwpJ1VfkbRZ6Ylx4yqg1igX0VCtapT-8P6WIQKwNnqsiEcAwk83EIhm6pxEjlx-k-WyVj_27boyV429IACtAuK9XcFSQ'
+		},
+		body: JSON.stringify()
+	})
+		.then(response => response.json());
+
 }
