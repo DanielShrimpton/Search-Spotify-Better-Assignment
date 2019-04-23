a6f4661e684ff2b4d4d97225c8bfe8f5ccdbf535
Daniel Shrimpton <44175578+DanielShrimpton@users.noreply.github.com>
2019-04-23 13:14:07 +0100

    Added more testing

--- app.test.js
+++ app.test.js
@@ -3,7 +3,8 @@
 const request = require('supertest');
 const app = require('./app');
 
-describe('Test page', () => {
+
+describe('Test the Home page', () => {
 
 	test('GET / succeeds', () => {
 
@@ -13,4 +14,66 @@ describe('Test page', () => {
 
 	});
 
+	test('GET / returns HTML', () => {
+
+		return request(app)
+			.get('/')
+			.expect('Content-type', /html/);
+
+	});
+
 });
+
+
+describe('Test the /details section', () => {
+
+	test('GET /details succeeds', () => {
+
+		return request(app)
+			.get('/details')
+			.expect(200);
+
+	});
+
+	test('GET /details return JSON', () => {
+
+		return request(app)
+			.get('/details')
+			.expect('Content-type', /json/);
+
+	});
+
+	test('GET /details includes details', () => {
+
+		return request(app)
+			.get('/details')
+			.expect(checkContent);
+
+	});
+
+});
+
+/**
+ * @param {*} res idk
+ */
+function checkContent(res) {
+
+	const jContent = res.body;
+	if (typeof jContent !== 'object') {
+
+		throw new Error('not an object');
+
+	}
+	if (jContent['display_name']) {
+
+		console.log(jContent['display_name']);
+
+	} else {
+
+		console.log(jContent['display_name']);
+		throw new Error('display_name should be the user');
+
+	}
+
+
+}
